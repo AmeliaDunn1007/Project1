6911140738c824fd714ff0930dc5665afe6aea99
AmeliaDunn1007 <mdunn62@bruinmail.slcc.edu>
2025-10-22 18:50:04 -0600

    Project 1

---
+++ Project1.py
@@ -0,0 +1,13 @@
+side = 8
+
+# addition = +
+# substraction = -
+# multiplication = *
+# division = /
+
+area = side * side
+
+fish = "tuna"
+animal = fish
+
+print("Side = " + str(side) + " Area =  " + str(area) + " Fish: " + fish + " Animal: " + animal)
